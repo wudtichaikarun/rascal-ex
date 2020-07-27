2f8ace2371bc2940b1932d07bf029002a98a142a
romantic <cesc_fabregas1414@hotmail.com>
2020-07-28 04:45:21 +0700

    simple queue remove unused code

--- src/simple/producer.js
+++
@@ -1,5 +0,0 @@
-import { broker } from '../libraries/amqp/index'
-;(async () => {
-  const publication = await broker.publish('demo_publication', 'Hello World!')
-  publication.on('error', console.error)
-})()
